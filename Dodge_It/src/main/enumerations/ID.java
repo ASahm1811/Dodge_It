32ac5d264a211fea5b4bdfb98c36ff8b45e5cd4c
Almir Šahman <38905710+almir013@users.noreply.github.com>
2021-12-02 00:06:51 +0100

    Remove dir

--- Dodge_It/src/main/enumerations/ID.java
+++
@@ -1,9 +0,0 @@
-package main.enumerations;
-
-public enum ID {
-    Player,
-    Enemy,
-    SmartEnemy,
-    BackGroundObject,
-    BlankObject
-}
--- Dodge_It/src/main/enumerations/STATE.java
+++
@@ -1,9 +0,0 @@
-package main.enumerations;
-
-public enum STATE {
-    MenuMode,
-    GameMode,
-    PauseMode,
-    EndGameMode,
-    HowToPlayMode
-}
